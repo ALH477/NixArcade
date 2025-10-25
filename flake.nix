e4349e7a8a17d5ea15bab6bbb96d64d1dfa1a3f4
Asher <146795551+ALH477@users.noreply.github.com>
2025-10-24 17:44:02 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 DeMoD LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy...
+# (full MIT text, or just "Licensed under the MIT License. See LICENSE for details.")
+
 {
   description = "NixArcade: The Arcade-Focused NixOS Distro from DeMoD LLC";
 
